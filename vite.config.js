5016714f8fcb849736f95d76f01b9a6e6a21e003
NightOne91 <104413024@stud.sju.edu.tw>
2026-05-19 02:01:36 +0800

    Bust Pages asset cache

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   base: './',
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-pagesfix-[hash].js',
+        chunkFileNames: 'assets/[name]-pagesfix-[hash].js',
+        assetFileNames: 'assets/[name]-pagesfix-[hash][extname]'
+      }
+    }
+  }
 })
